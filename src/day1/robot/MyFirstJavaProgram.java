555b44523076387e031a46d80fecae0987d373f6
RhymeLyet <league@WTS14.attlocal.net>
2017-07-30 15:27:22 -0700

    firstly

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,8 +7,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot r2d2 = new Robot ();
 
-	
+for (int i = 0; i < 50 ; i++) {
+	r2d2 .penDown();
+	r2d2 .move(50);
+	r2d2 .turn(5);
+
+}
 		
 		
 	}
